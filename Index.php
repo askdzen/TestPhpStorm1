c15f71cca724d73ffc8964e74b45116664c006f2
askdzen <ask.dzen@mail.ru>
2017-12-05 13:39:04 +0600

    051217

---
+++ Index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/main.css" >
+</head>
+<body>
+<p>
+    fsdfgdgdfgdfg
+</p>
+1ё
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Programmist1c
+ * Date: 21.06.2017
+ * Time: 15:03
+ */
+//echo "fddfhd";
+//echo 2+3;
+//echo "<a href=\"php/Index.html\" >go</a>";
+//echo "<a href=\"php/test.php\" >go</a>";
+$arr = [ 1, 2+3, 3, "Вот" ];
+var_dump( $arr );
+$c = test(5,6);
+
+function test($a,$b){
+return $a+$b;
+}
+echo $c;
+?>
+</body>
+</html>
+
---
+++ php/test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../css/main.css" >
+</head>
+<body>
+<p>
+    fsdfgdgdfgdfg
+</p>
+<?php
+echo "Вася";
+?>
+
+</body>
+</html>
