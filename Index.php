9e61be2c95c33cfc5a750b35561a3515dd8b0b63
askdzen <ask.dzen@mail.ru>
2017-12-05 14:04:39 +0600

    Merge branch 'master' of D:\!\OpenServer\OpenServer\domains\localhost\TestPhpStorm1 with conflicts.

--- Index.php
+++ Index.php
@@ -4,21 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="css/main.css" >
-=======
     <link rel="stylesheet" href="../css/main.css" >
->>>>>>> origin/master
 </head>
 <body>
 <p>
     fsdfgdgdfgdfg
 </p>
-<<<<<<< HEAD
-1ё
-=======
 
->>>>>>> origin/master
 <?php
 /**
  * Created by PhpStorm.
@@ -30,25 +22,25 @@
 //echo 2+3;
 //echo "<a href=\"php/Index.html\" >go</a>";
 //echo "<a href=\"php/test.php\" >go</a>";
-<<<<<<< HEAD
-$arr = [ 1, 2+3, 3, "Вот" ];
+$arr = [ 1, 2, 3 ];
 var_dump( $arr );
-$c = test(5,6);
-
-function test($a,$b){
-return $a+$b;
-}
-echo $c;
-?>
-=======
-//$arr = [ 1, 2, 3 ];
-//var_dump( $arr );
 ?>
 
 <script type="text/javascript">
     document.write("Today is " + Date() );
+    onerror = errorHandler
+    //document.writ("Добро пожаловать на этот сайт!") // Преднамеренная ошибка
+    function errorHandler(message, url, line)
+    {
+        out = "К сожалению, обнаружена ошибка.\n\n";
+        out += "Ошибка: " + message + "\n";
+        out += "URL: " + url + "\n";
+        out += "Строка: " + line + "\n\n";
+        out += "Щелкните на кнопке OK для продолжения работы.\n\n";
+        alert(out);
+        return true;
+    }
 </script>
->>>>>>> origin/master
 </body>
 </html>
 
